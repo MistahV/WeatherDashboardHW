77585885da709ded59aae9bdbd2d582f572f044d
chrisvanetten <christophervanetten@gmail.com>
2021-08-18 14:47:06 -0500

    restructured API function to make two API calls - transferring city name to city coords

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -14,7 +14,7 @@ function handleSearchFormSubmit(event) {
     return;
   }
 
-  searchApi(searchInputVal);
+  searchApiWeather(searchInputVal);
 }
 
 
@@ -30,62 +30,76 @@ function handleSearchFormSubmit(event) {
 // }
 
 
-function searchApi() {
 
-// One Call Weather API uses lon & lat coordinates for search...
 
-// So, use original API to get city coordinates from user search input, then pass those coordinates into One Call API
+   // One Call Weather API uses lon & lat coordinates for search...
+    
+    // So, use original API to get city coordinates from user search input, then pass those coordinates into One Call API
+    
 
-  var requestURLCity = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=d6b1c98ae6030744047ac8cf7d9346ba`;
+function searchApiWeather() {
+
+
+    var requestURLCity = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=d6b1c98ae6030744047ac8cf7d9346ba`;
+      
+     fetch(requestURLCity)
+        .then(response => response.json())
+        .then(data => {
+           let cityLon = (data.coord.lon);
+           let cityLat = (data.coord.lat);
+
+           console.log(cityLon);
+           console.log(cityLat);
+
+           return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLon}&lon=${cityLat}&exclude=minutely,hourly&appid=d6b1c98ae6030744047ac8cf7d9346ba`)
+              .then(response => response.json())
+              .then(data => {
+                  let weatherInfo = (data.current);
+                  console.log(weatherInfo)
+
+                  return weatherInfo;
+              })
+        })
+ }
   
-  fetch(requestURLCity)
-    .then(response => response.json())
-    .then(data => {
-       let cityLon = (data.coord);
-       let cityLat = (data.coord);
-
-       console.log(cityLon);
-       console.log(cityLan)
-  })
-
-
-  var requestUrlWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly&appid=d6b1c98ae6030744047ac8cf7d9346ba`;
-
-  fetch(requestUrlWeather)
-    .then(response => response.json())
-    .then(data => {
-       let weatherInfo = (data.results);
-       console.log(weatherInfo)
-  })
+   
+ 
+ 
+  // fetch(requestUrlWeather)
+  //   .then(response => response.json())
+  //   .then(data => {
+  //      let weatherInfo = (data.results);
+  //      console.log(weatherInfo)
+  // })
   
-    .catch(function (error) {
-      console.error(error);
-    });
+  //   .catch(function (error) {
+  //     console.error(error);
+  //   });
+
+
+
+function showWeather () {
+  
+
+  // set up `<div>` to hold result content
+  var resultCard = document.createElement('div');
+  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+
+  var resultBody = document.createElement('div');
+  resultBody.classList.add('card-body');
+  resultCard.append(resultBody);
+
+  var titleEl = document.createElement('h3');
+  titleEl.textContent = resultObj.title;
+
+  var bodyContentEl = document.createElement('p');
+  bodyContentEl.innerHTML = 
+  
+
+  resultBody.append(titleEl, bodyContentEl);
+
+  resultContentEl.append(resultCard);
 }
-
-
-// function showWeather (resultObj) {
-//   console.log(resultObj);
-
-//   // set up `<div>` to hold result content
-//   var resultCard = document.createElement('div');
-//   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
-
-//   var resultBody = document.createElement('div');
-//   resultBody.classList.add('card-body');
-//   resultCard.append(resultBody);
-
-//   var titleEl = document.createElement('h3');
-//   titleEl.textContent = resultObj.title;
-
-//   var bodyContentEl = document.createElement('p');
-//   bodyContentEl.innerHTML = 
-  
-
-//   resultBody.append(titleEl, bodyContentEl);
-
-//   resultContentEl.append(resultCard);
-// }
 
 
 
