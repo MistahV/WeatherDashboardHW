5cb7fe56e332707297c939ecf4bb6c01f4db2794
chrisvanetten <christophervanetten@gmail.com>
2021-08-18 13:45:02 -0500

    tweaked CSS scheme, cleaned up JS, made progress on API call

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -1,114 +1,96 @@
-var resultTextEl = document.querySelector('#result-text');
-var resultContentEl = document.querySelector('#result-content');
-var searchFormEl = document.querySelector('#search-form');
+var resultContentEl = document.querySelector('#cityWeather-container');
+var searchFormEl = document.querySelector('#user-form');
+var searchInput = document.querySelector('#searchCity')
 
-function getParams() {
-  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-  var searchParamsArr = document.location.search.split('&');
 
-  // Get the query and format values
-  var query = searchParamsArr[0].split('=').pop();
-  var format = searchParamsArr[1].split('=').pop();
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+ 
+  var searchInputVal = searchInput.value;
+  console.log(searchInputVal)
 
-  searchApi(query, format);
+  if (!searchInputVal) {
+    alert('You need to correctly enter a city name!');
+    return;
+  }
+
+  searchApi(searchInputVal);
 }
 
-function printResults(resultObj) {
-  console.log(resultObj);
 
-  // set up `<div>` to hold result content
-  var resultCard = document.createElement('div');
-  resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
+// function getParams() {
+//   // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+//   var searchParamsArr = document.location.search.split('&');
 
-  var resultBody = document.createElement('div');
-  resultBody.classList.add('card-body');
-  resultCard.append(resultBody);
+//   // Get the query and format values
+//   var query = searchParamsArr[0].split('=').pop();
+//   var format = searchParamsArr[1].split('=').pop();
 
-  var titleEl = document.createElement('h3');
-  titleEl.textContent = resultObj.title;
+//   searchApi(query, format);
+// }
 
-  var bodyContentEl = document.createElement('p');
-  bodyContentEl.innerHTML =
-    '<strong>Date:</strong> ' + resultObj.date + '<br/>';
 
-  if (resultObj.subject) {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> ' + resultObj.subject.join(', ') + '<br/>';
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Subjects:</strong> No subject for this entry.';
-  }
+function searchApi() {
 
-  if (resultObj.description) {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong> ' + resultObj.description[0];
-  } else {
-    bodyContentEl.innerHTML +=
-      '<strong>Description:</strong>  No description for this entry.';
-  }
+// One Call Weather API uses lon & lat coordinates for search...
 
-  var linkButtonEl = document.createElement('a');
-  linkButtonEl.textContent = 'Read More';
-  linkButtonEl.setAttribute('href', resultObj.url);
-  linkButtonEl.classList.add('btn', 'btn-dark');
+// So, use original API to get city coordinates from user search input, then pass those coordinates into One Call API
 
-  resultBody.append(titleEl, bodyContentEl, linkButtonEl);
+  var requestURLCity = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=d6b1c98ae6030744047ac8cf7d9346ba`;
+  
+  fetch(requestURLCity)
+    .then(response => response.json())
+    .then(data => {
+       let cityLon = (data.coord);
+       let cityLat = (data.coord);
 
-  resultContentEl.append(resultCard);
-}
+       console.log(cityLon);
+       console.log(cityLan)
+  })
 
-function searchApi(query, format) {
-  var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
 
-  if (format) {
-    locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
-  }
+  var requestUrlWeather = `https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly&appid=d6b1c98ae6030744047ac8cf7d9346ba`;
 
-  locQueryUrl = locQueryUrl + '&q=' + query;
-
-  fetch(locQueryUrl)
-    .then(function (response) {
-      if (!response.ok) {
-        throw response.json();
-      }
-
-      return response.json();
-    })
-    .then(function (locRes) {
-      // write query to page so user knows what they are viewing
-      resultTextEl.textContent = locRes.search.query;
-
-      console.log(locRes);
-
-      if (!locRes.results.length) {
-        console.log('No results found!');
-        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-      } else {
-        resultContentEl.textContent = '';
-        for (var i = 0; i < locRes.results.length; i++) {
-          printResults(locRes.results[i]);
-        }
-      }
-    })
+  fetch(requestUrlWeather)
+    .then(response => response.json())
+    .then(data => {
+       let weatherInfo = (data.results);
+       console.log(weatherInfo)
+  })
+  
     .catch(function (error) {
       console.error(error);
     });
 }
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
 
-  var searchInputVal = document.querySelector('#search-input').value;
-  var formatInputVal = document.querySelector('#format-input').value;
+// function showWeather (resultObj) {
+//   console.log(resultObj);
 
-  if (!searchInputVal) {
-    console.error('You need a search input value!');
-    return;
-  }
+//   // set up `<div>` to hold result content
+//   var resultCard = document.createElement('div');
+//   resultCard.classList.add('card', 'bg-light', 'text-dark', 'mb-3', 'p-3');
 
-  searchApi(searchInputVal, formatInputVal);
-}
+//   var resultBody = document.createElement('div');
+//   resultBody.classList.add('card-body');
+//   resultCard.append(resultBody);
+
+//   var titleEl = document.createElement('h3');
+//   titleEl.textContent = resultObj.title;
+
+//   var bodyContentEl = document.createElement('p');
+//   bodyContentEl.innerHTML = 
+  
+
+//   resultBody.append(titleEl, bodyContentEl);
+
+//   resultContentEl.append(resultCard);
+// }
+
+
+
+
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
-getParams();
+// getParams();
